2ecd4fa18f42cef1fec70d9e59968150998e2cb5
Alexander Lichter <manniL@gmx.net>
2018-10-30 15:59:36 +0000

    feat(warning): add consola warning for generate and SPA build mode

--- test/module.test.js
+++ test/module.test.js
@@ -1,10 +1,71 @@
-const { Nuxt } = require('nuxt-edge')
-const request = require('request-promise-native')
+const { Nuxt, Generator, Builder } = require('nuxt-edge')
+const consola = require('consola')
 
+const request = require('request-promise-native')
 const config = require('./fixture/nuxt.config')
 
 const url = path => `http://localhost:3000${path}`
 const get = path => request(url(path))
+
+describe('warnings', () => {
+  let nuxt
+  let log
+
+  beforeEach(() => {
+    log = jest.fn()
+    consola.clear().add({ log })
+  })
+
+  afterEach(async () => {
+    await nuxt.close()
+  })
+
+  it('spa mode', async () => {
+    nuxt = new Nuxt(
+      Object.assign({}, config, {
+        render: {
+          ssr: false
+        },
+        proxy: {
+          '/proxy': url('/api'),
+          '/rewrite': {
+            target: url('/api'),
+            pathRewrite: { '^/rewrite': '' }
+          }
+        }
+      })
+    )
+    await nuxt.listen(3000)
+
+    const messageInExtendFunction = 'This module does only work in SSR mode'
+    const consolaMessages = log.mock.calls.map(c => c[0].message)
+    expect(consolaMessages).toContain(messageInExtendFunction)
+  })
+
+  it('generate', async () => {
+    nuxt = new Nuxt(
+      Object.assign({}, config, {
+        render: {
+          ssr: false
+        },
+        proxy: {
+          '/proxy': url('/api'),
+          '/rewrite': {
+            target: url('/api'),
+            pathRewrite: { '^/rewrite': '' }
+          }
+        }
+      })
+    )
+    const generator = new Generator(nuxt, new Builder(nuxt))
+    await generator.initiate()
+    await generator.initRoutes()
+
+    const messageInExtendFunction = 'This module does only work in SSR mode'
+    const consolaMessages = log.mock.calls.map(c => c[0].message)
+    expect(consolaMessages).toContain(messageInExtendFunction)
+  }, 30000)
+})
 
 describe('object mode', () => {
   let nuxt
