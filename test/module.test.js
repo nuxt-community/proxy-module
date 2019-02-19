8baafc9912de56761217f37994a5415230381b5d
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-02-19 16:06:52 -0300

    fix(module): generate warn if not SPA and increase coverage (#31)

--- test/module.test.js
+++ test/module.test.js
@@ -9,62 +9,74 @@ config.dev = false
 
 let nuxt
 
+consola.mockTypes(() => jest.fn())
+
 const url = path => `http://localhost:3000${path}`
 const get = path => request(url(path))
 
 describe('warnings', () => {
-  let log
-
   beforeEach(() => {
-    log = jest.fn()
-    consola.clear().add({ log })
+    consola.warn.mockClear()
   })
 
   afterEach(async () => {
     await nuxt.close()
   })
 
-  test.skip('generate', async () => {
-    nuxt = new Nuxt(
-      Object.assign({}, config, {
-        render: {
-          ssr: false
-        },
-        build: {
-          quiet: false
-        },
-        proxy: {
-          '/proxy': url('/api'),
-          '/rewrite': {
-            target: url('/api'),
-            pathRewrite: { '^/rewrite': '' }
-          }
+  test('generate universal mode', async () => {
+    nuxt = new Nuxt({
+      ...config,
+      render: {
+        ssr: false
+      },
+      build: {
+        quiet: true
+      },
+      proxy: {
+        '/proxy': url('/api'),
+        '/rewrite': {
+          target: url('/api'),
+          pathRewrite: { '^/rewrite': '' }
         }
-      })
-    )
+      }
+    })
+
     const generator = new Generator(nuxt, new Builder(nuxt))
     await generator.initiate()
     await generator.initRoutes()
 
-    const messageInExtendFunction = 'The module `@nuxtjs/proxy` does not work in generated mode.'
-    const consolaMessages = log.mock.calls.map(c => c[0].message)
-    expect(consolaMessages).toContain(messageInExtendFunction)
+    expect(consola.warn).toHaveBeenNthCalledWith(1, 'The module `@nuxtjs/proxy` does not work in generated mode.')
+  })
+
+  test('generate spa mode', async () => {
+    nuxt = new Nuxt({
+      ...config,
+      mode: 'spa',
+      build: {
+        quiet: true
+      }
+    })
+
+    const generator = new Generator(nuxt, new Builder(nuxt))
+    await generator.initiate()
+    await generator.initRoutes()
+
+    expect(consola.warn).not.toHaveBeenCalled()
   })
 })
 
 describe('object mode', () => {
   beforeAll(async () => {
-    nuxt = new Nuxt(
-      Object.assign({}, config, {
-        proxy: {
-          '/proxy': url('/api'),
-          '/rewrite': {
-            target: url('/api'),
-            pathRewrite: { '^/rewrite': '' }
-          }
+    nuxt = new Nuxt({
+      ...config,
+      proxy: {
+        '/proxy': url('/api'),
+        '/rewrite': {
+          target: url('/api'),
+          pathRewrite: { '^/rewrite': '' }
         }
-      })
-    )
+      }
+    })
 
     await nuxt.listen(3000)
   })
@@ -77,18 +89,40 @@ describe('object mode', () => {
     expect(await get('/proxy/aaa')).toBe('url:/proxy/aaa')
   })
 
-  it('pathRewrite', async () => {
+  test('pathRewrite', async () => {
     expect(await get('/rewrite/aaa')).toBe('url:/aaa')
   })
 })
 
 describe('array mode', () => {
   beforeAll(async () => {
-    nuxt = new Nuxt(
-      Object.assign({}, config, {
-        proxy: [url('/api')]
-      })
-    )
+    nuxt = new Nuxt({
+      ...config,
+      proxy: [
+        url('/api'),
+        [url('/api'), {}]
+      ]
+    })
+
+    await nuxt.listen(3000)
+  })
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  test('basic', async () => {
+    await expect(await get('/proxy/aaa')).toBe('url:/proxy/aaa')
+  })
+})
+
+describe('disabled', () => {
+  beforeAll(async () => {
+    nuxt = new Nuxt({
+      ...config,
+      proxy: false
+    })
+
     await nuxt.listen(3000)
   })
 
